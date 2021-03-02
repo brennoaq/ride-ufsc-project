b5ff4bd3f6dd638cd4bf709b9ab23783d43abb1b
Arthur Sady <arthursady@jungledevs.com>
2021-03-02 16:01:57 -0300

    feat: Add podfile to flutter boilerplate

--- setup.sh
+++ setup.sh
@@ -9,19 +9,20 @@ cp -r ./ "../$newname"
 cd "../$newname"
 mv README-TEMPLATE.md README.md
 
-# oldname="boilerplate-ios"
-# sed -i "" "s/$oldname/$newname/g" ./project.yml
-# sed -i "" "s/$oldname/$newname/g" ./Podfile
-# find . -type f -name "Podfile" -exec sed -i "" "/target 'junglerocks-ios' do/,/end/d" {} \; #Remove junglerocks-ios mentions on Podfile
-# sed -i "" "s/$oldname/$newname/g" ./fastlane/Fastfile
-# find . -type f -name "*.swift" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on swift files
-# find . -type f -name "*.strings" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on string files
-# find . -type f -name "*.xcconfig" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on config files
-# sed -i "" "s/$oldname/$newname/g" ./README.md
-
-# testOldName="boilerplate_ios"
-# find . -type f -name "*.swift" -exec sed -i "" "s/$testOldName/$testNewName/g" {} \; #Rename boilerplate name on test files
 cd "ios/"
+pod init
+
+oldname="boilerplate-ios"
+sed -i "" "s/$oldname/$newname/g" ./project.yml
+sed -i "" "s/$oldname/$newname/g" ./Podfile
+find . -type f -name "*.swift" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on swift files
+find . -type f -name "*.strings" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on string files
+find . -type f -name "*.xcconfig" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on config files
+sed -i "" "s/$oldname/$newname/g" ./README.md
+
+testOldName="boilerplate_ios"
+find . -type f -name "*.swift" -exec sed -i "" "s/$testOldName/$testNewName/g" {} \; #Rename boilerplate name on test files
+
 rm -rf .git *.xcworkspace *.xcodeproj XcodeTemplates/
 rm Podfile.lock
 rm setup.sh
