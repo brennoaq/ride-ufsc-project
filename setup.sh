b01140c6c12f545c2d6ff355d8e4d9f0b84574b8
Arthur Sady <arthursady@jungledevs.com>
2021-03-07 11:05:01 -0300

    feat: Update find and replace strings

--- setup.sh
+++ setup.sh
@@ -1,7 +1,22 @@
-echo "\n----- Starting a new iOS application -----\n\n"
-echo "Enter app name (e.g. new-app): "
+echo "\n----- Starting a new Flutter application -----\n\n"
+echo "Enter project repo name: "
 read newname
 testNewName=${newname//-/_} #replace "-" characters with "_"
+
+echo "\n----- Change bundleId -----\n\n"
+echo "Enter bundleId: "
+read newbundleID
+
+oldBundleID="com.boilerplate"
+
+#  App name
+
+echo "\n----- Change App Display Name  -----\n\n"
+echo "Enter AppName: "
+read newAppName
+oldAppName="BoilerPlateNameReplace"
+
+# iOS
 
 rm -rf Pods/
 mkdir "../$newname"
@@ -11,17 +26,17 @@ mv README-TEMPLATE.md README.md
 
 cd "ios/"
 
-oldname="boilerplate-ios"
-sed -i "" "s/$oldname/$newname/g" ./project.yml
-sed -i "" "s/$oldname/$newname/g" ./Podfile
-find . -type f -name "*.swift" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on swift files
-find . -type f -name "*.strings" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on string files
-find . -type f -name "*.xcconfig" -exec sed -i "" "s/$oldname/$newname/g" {} \; #Rename boilerplate mentions on config files
-sed -i "" "s/$oldname/$newname/g" ./README.md
-
-testOldName="boilerplate_ios"
-find . -type f -name "*.swift" -exec sed -i "" "s/$testOldName/$testNewName/g" {} \; #Rename boilerplate name on test files
+find . -type f -name "*.xcconfig" -exec sed -i "" "s/$oldAppName/$newAppName/g" {} \; #Rename boilerplate mentions on config files
+find . -type f -name "*.xcconfig" -exec sed -i "" "s/$oldBundleID/$newbundleID/g" {} \; #Rename boilerplate mentions on config files
+sed -i "" "s/$oldAppName/$newAppName/g" ./README.md
 
 rm Podfile.lock
 rm setup.sh
 pod install
+cd ..
+
+# Android 
+cd android/app
+
+sed -i "" "s/$oldBundleID/$newbundleID/g" ./build.gradle
+sed -i "" "s/$oldAppName/$newAppName/g" ./build.gradle
