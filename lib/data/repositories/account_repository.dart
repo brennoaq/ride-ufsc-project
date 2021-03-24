7cf6f99669a0a715490e1fa1e0bdf08e408b74b7
Arthur Sady <arthursady@jungledevs.com>
2021-03-24 14:59:11 -0300

    feat: Add user subject to account repository

--- lib/data/repositories/account_repository.dart
+++ lib/data/repositories/account_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:boilerplate_flutter/data/api/user_api.dart';
 import 'package:boilerplate_flutter/data/models/user_model.dart';
 import 'package:rxdart/rxdart.dart';
@@ -6,11 +8,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AccountRepository {
   AccountRepository() {
     _getToken().then((value) => tokenSubject.add(value));
+    _getUser().then((value) => userSubject.add(value));
   }
 
   BehaviorSubject<String> tokenSubject = BehaviorSubject();
+  BehaviorSubject<UserModel> userSubject = BehaviorSubject();
 
   String get token => tokenSubject.value;
+  UserModel get currentUser => userSubject.value;
 
   Future<Exception> login({String email, String password}) async {
     try {
@@ -31,6 +36,11 @@ class AccountRepository {
     return prefs.getString('token');
   }
 
+  Future<UserModel> _getUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    return UserModel.fromJSON(jsonDecode(prefs.getString('user')));
+  }
+
   Future<void> createSession(String token, UserModel user) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('token', token);
@@ -38,6 +48,7 @@ class AccountRepository {
   }
 
   Future<void> updateUser(UserModel user) async {
+    userSubject.add(user);
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('user', user.jsonString);
   }
