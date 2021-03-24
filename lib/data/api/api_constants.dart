4a21434724d595035aece281ac5bff61a5ed29ce
Arthur Sady <arthursady@jungledevs.com>
2021-03-24 11:15:09 -0300

    feat: Integrate login with error states

--- lib/data/api/api_constants.dart
+++ lib/data/api/api_constants.dart
@@ -1,7 +1,9 @@
-abstract class EndPoints {
+class ApiConstants {
   static const _baseUrl = 'https://api-staging.jungle.rocks';
   // Login -> email: " dev@jungledevs.com ", password: " developer123"
   static String login() {
     return '$_baseUrl/api/v1/login/';
   }
+
+  static List<String> errorIds = ['non_field_errors'];
 }
--- lib/data/repositories/account_repository.dart
+++ lib/data/repositories/account_repository.dart
@@ -1,5 +1,5 @@
+import 'package:boilerplate_flutter/data/api/user_api.dart';
 import 'package:boilerplate_flutter/data/models/user_model.dart';
-import 'package:boilerplate_flutter/util/api/user_api.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,16 +13,16 @@ class AccountRepository {
   String get token => tokenSubject.value;
 
   Future<Exception> login({String email, String password}) async {
-    var loginResponse = await UserApi.login(email, password);
-    if (loginResponse != null) {
-      try {
+    try {
+      var loginResponse = await UserApi.login(email, password);
+      if (loginResponse != null) {
         await createSession(loginResponse.token, loginResponse.user);
-      } catch (error) {
-        return Exception(error.toString());
+        return null;
       }
-
-      return null;
+    } catch (error) {
+      return Exception(error.toString());
     }
+
     return Exception('Something went wrong');
   }
 
